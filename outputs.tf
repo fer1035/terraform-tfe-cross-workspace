be8ff3c10e731c0a67ca8272f9a39108b6c3156d
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2023-05-03 20:25:11 +0800

    Fixed module code and updated README.

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,14 @@
-data "tfe_workspace" "workspace_0" {
-  name         = var.workspace_0_name
-  organization = var.org_name
-}
-
-data "tfe_workspace" "workspace_1" {
-  name         = var.workspace_1_name
-  organization = var.org_name
-}
-
-data "tfe_workspace" "workspace_2" {
-  name         = var.workspace_2_name
-  organization = var.org_name
-}
-
 output "workspace_0" {
   description = "Workspace 0 data (map)."
-  value       = data.tfe_workspace.workspace_0
+  value       = tfe_workspace.workspace_0
 }
 
 output "workspace_1" {
   description = "Workspace 1 data (map)."
-  value       = data.tfe_workspace.workspace_1
+  value       = tfe_workspace.workspace_1
 }
 
 output "workspace_2" {
   description = "Workspace 2 data (map)."
-  value       = data.tfe_workspace.workspace_2
+  value       = tfe_workspace.workspace_2
 }
