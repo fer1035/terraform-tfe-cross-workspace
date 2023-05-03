6c203eee704c5719218a352805d80c1bd50c3c81
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2023-05-03 21:38:39 +0800

    Fixed project assignment.

--- project.tf
+++ project.tf
@@ -3,6 +3,11 @@ data "tfe_variable_set" "variable_set" {
   organization = var.org_name
 }
 
+data "tfe_team" "team" {
+  name         = var.team_name
+  organization = var.org_name
+}
+
 resource "tfe_project" "project" {
   name         = var.project_name
   organization = var.org_name
@@ -11,4 +16,12 @@ resource "tfe_project" "project" {
 resource "tfe_project_variable_set" "project_variable_set" {
   variable_set_id = data.tfe_variable_set.variable_set.id
   project_id      = tfe_project.project.id
+}
+
+resource "tfe_team_project_access" "project_access" {
+  count = var.team_name == "owners" ? 0 : 1
+
+  access       = "write"
+  team_id      = data.tfe_team.team.id
+  project_id   = tfe_project.project.id
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "org_name" {
   type        = string
-  description = "Name of an existing organization to use."
+  description = "Name of an existing Organization to use."
 }
 
 variable "varset_name" {
@@ -10,31 +10,26 @@ variable "varset_name" {
 
 variable "project_name" {
   type        = string
-  description = "Name of the project to create and / or manage. Only a-z, A-Z, and 0-9 are allowed for the project name, beginning with a letter."
-
-  validation {
-    condition     = can(regex("^[A-Za-z]+[A-Za-z0-9]*$", var.project_name))
-    error_message = "Only a-z, A-Z, and 0-9 are allowed for the project name, beginning with a letter."
-  }
+  description = "Name of the Project to create and / or manage."
 }
 
 variable "workspace_0_name" {
   type        = string
-  description = "Name of the 0th workspace to create and / or manage."
+  description = "Name of the 0th Workspace to create and / or manage."
 }
 
 variable "workspace_1_name" {
   type        = string
-  description = "Name of the 1st workspace to create and / or manage."
+  description = "Name of the 1st Workspace to create and / or manage."
 }
 
 variable "workspace_2_name" {
   type        = string
-  description = "Name of the 2nd workspace to create and / or manage."
+  description = "Name of the 2nd Workspace to create and / or manage."
 }
 
 variable "team_name" {
   type        = string
-  description = "Name of an existing team to assign to the workspaces. The default \"owners\" team does not need Team Access assignment, and this condition has been configured automatically in the code."
+  description = "Name of an existing Team to assign to the Workspaces. The default \"owners\" Team does not need Team Access assignments, and this condition has been configured automatically in the code."
   default     = "owners"
 }
