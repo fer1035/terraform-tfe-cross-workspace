436aed0238d2948d8aec431de35d62c13c56f219
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2023-05-04 16:57:25 +0800

    Updated README and examples.

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,5 +1,5 @@
 module "cross-workspace" {
-  source = "app.terraform.io/fer1035/cross-workspace/tfe"
+  source = "app.terraform.io/my-org/cross-workspace/tfe"
 
   org_name         = "my-org"
   project_name     = "my-project"
@@ -7,4 +7,16 @@ module "cross-workspace" {
   workspace_0_name = "my-workspace-0"
   workspace_1_name = "my-workspace-1"
   workspace_2_name = "my-workspace-2"
+}
+
+output "my-workspace-0" {
+  value = module.cross-workspace.workspace_0
+}
+
+output "my-workspace-1" {
+  value = module.cross-workspace.workspace_1
+}
+
+output "my-workspace-2" {
+  value = module.cross-workspace.workspace_2
 }
